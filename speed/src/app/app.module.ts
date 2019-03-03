04be9abc7a5af2f7d891fcb36d56e411ba6a2100
alfredoPiris <47152905+alfredoPiris@users.noreply.github.com>
2019-03-03 18:35:20 +0100

    Entrega

--- speed/src/app/app.module.ts
+++ speed/src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusquedaContainerComponent } from './busqueda-container/busqueda-container.component';
+import { BusquedaAgenciasPresenterComponent } from './busqueda-agencias-presenter/busqueda-agencias-presenter.component';
+import { ResultadoBusquedaAgenciasPresenterComponent } from './resultado-busqueda-agencias-presenter/resultado-busqueda-agencias-presenter.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { BusquedaEffects } from './reducers/busqueda.effects';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusquedaContainerComponent,
+    BusquedaAgenciasPresenterComponent,
+    ResultadoBusquedaAgenciasPresenterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([BusquedaEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
